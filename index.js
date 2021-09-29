b7689e4bf1d6ac90b3742d929438ad128c367a73
rohit8020 <rohitgangwar8020@gmail.com>
2021-09-29 11:24:23 +0530

    creating div js

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-let items=document.querySelectorAll(".item")
+let container=document.querySelector(".container")
 
-items.forEach(item=>{
+const num_of_div=5000
+
+for(let i=0;i<num_of_div;i++){
+    let div=document.createElement("div")
+    container.appendChild(div)
+}
+
+
+
+container.childNodes.forEach(item=>{
     item.addEventListener('mousemove', ()=>{
         item.style.backgroundColor=`rgb(${Math.random()*256},${Math.random()*256},${Math.random()*256})`
         setTimeout(()=>{item.style.backgroundColor="transparent"},400)
